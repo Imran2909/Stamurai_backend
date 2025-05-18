4590c93a0850a19f67c2c3a91616e07ff736b451
Imran Sutar <112754652+Imran2909@users.noreply.github.com>
2025-05-19 00:39:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 app.use(cors({
   origin: true,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Cookie"]
 }));
 
